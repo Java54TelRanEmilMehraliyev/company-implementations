2a4490c74d2c70f7de292dbbaf6c4b472a9e7c8b
user.emial <emil.mehraliyev003@gmail.com>
2024-07-14 21:31:50 +0300

    HW 36

---
+++ src/test/java/telran/employees/CompanyMapsImplTest.java
@@ -0,0 +1,19 @@
+package telran.employees;
+
+
+import org.junit.jupiter.api.BeforeEach;
+
+public class CompanyMapsImplTest extends CompanyTest {
+	@Override
+	@BeforeEach
+	void setCompany() {
+		company = getEmptyCompany();
+		super.setCompany();
+	}
+
+	@Override
+	protected Company getEmptyCompany() {
+		
+		return new CompanyMapsImpl();
+	}
+}
